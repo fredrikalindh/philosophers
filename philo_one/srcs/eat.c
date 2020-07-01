44d7a0ff86a7eb7e653e84245b9563505cfc675d
Fredrika Lindh <fredrikalindh@Fredrikas-MacBook-Pro.local>
2020-07-01 23:16:58 +0200

    sa

--- philo_one/srcs/eat.c
+++ philo_one/srcs/eat.c
@@ -14,14 +14,18 @@
 
 void	eat(t_phil *phil)
 {
-	while (!phil->info->n_forks[phil->name - 1] && 
+	while (1)
+	{
+		if (!phil->info->n_forks[phil->name - 1] && 
 		!phil->info->n_forks[phil->name % phil->info->num_phil])
-		;
-	phil->info->n_forks[phil->name - 1] = 0;
-	phil->info->n_forks[phil->name % phil->info->num_phil] = 0;
-	pthread_mutex_lock(phil->f1);
-	pthread_mutex_lock(phil->f2);
-	message(phil, FORK);
+		{
+			phil->info->n_forks[phil->name - 1] = 0;
+			phil->info->n_forks[phil->name % phil->info->num_phil] = 0;
+			pthread_mutex_lock(phil->f1);
+			pthread_mutex_lock(phil->f2);
+			message(phil, FORK);
+		}
+	}
 	if (!phil->info->someone_is_dead)
 	{
 		pthread_mutex_lock(&phil->is_eating);
@@ -37,3 +41,28 @@ void	eat(t_phil *phil)
 	pthread_mutex_unlock(phil->f1);
 	pthread_mutex_unlock(phil->f2);
 }
+// void	eat(t_phil *phil)
+// {
+// 	while (!phil->info->n_forks[phil->name - 1] && 
+// 		!phil->info->n_forks[phil->name % phil->info->num_phil])
+// 		;
+// 	phil->info->n_forks[phil->name - 1] = 0;
+// 	phil->info->n_forks[phil->name % phil->info->num_phil] = 0;
+// 	pthread_mutex_lock(phil->f1);
+// 	pthread_mutex_lock(phil->f2);
+// 	message(phil, FORK);
+// 	if (!phil->info->someone_is_dead)
+// 	{
+// 		pthread_mutex_lock(&phil->is_eating);
+// 		message(phil, FORK);
+// 		message(phil, EAT);
+// 		phil->last_eat = get_time();
+// 		real_sleep(phil->info->time_to_eat);
+// 		phil->info->n_forks[phil->name - 1] = 1;
+// 		phil->info->n_forks[phil->name % phil->info->num_phil] = 1;
+// 		++phil->times_eaten;
+// 		pthread_mutex_unlock(&phil->is_eating);
+// 	}
+// 	pthread_mutex_unlock(phil->f1);
+// 	pthread_mutex_unlock(phil->f2);
+// }
