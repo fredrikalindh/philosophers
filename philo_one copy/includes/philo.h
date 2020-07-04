a1120deb323743b7bbc56d5c694e33ba61e80075
Fredrika Lindh <fredrikalindh@Fredrikas-MacBook-Pro.local>
2020-07-04 11:40:06 +0200

    fuck this :)

---
+++ philo_one copy/includes/philo.h
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fredrika <fredrika@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/08 16:39:25 by fredrika          #+#    #+#             */
+/*   Updated: 2020/03/27 13:52:43 by fredrikalindh    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <ft_printf.h>
+# include <stdbool.h>
+
+typedef enum			e_messages
+{
+	FORK,
+	EAT,
+	SLEEP,
+	THINK,
+	DEAD,
+	ENOUGH
+}						t_messages;
+
+typedef struct			s_info
+{
+	int					someone_is_dead:1;
+	pthread_mutex_t		write;
+	pthread_mutex_t		fork_check;
+	pthread_mutex_t		*forks;
+	bool				*n_forks;
+	int					num_phil;
+	int					phils_whos_eaten_enough;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					max_eat;
+	int					start:1;
+}						t_info;
+
+typedef struct			s_phil
+{
+	int					name;
+	unsigned long long	last_eat;
+	int					times_eaten;
+	pthread_mutex_t		is_eating;
+	pthread_mutex_t		*f1;
+	pthread_mutex_t		*f2;
+	t_info				*info;
+}						t_phil;
+
+typedef struct			s_list
+{
+	void				*data;
+	struct s_list		*next;
+}						t_list;
+
+int						free_all_malloc(void);
+int						ft_atoi(char *str);
+void					*mmalloc(unsigned int size);
+void					eat(t_phil *phil);
+pthread_t				*start_program(t_info *info);
+u_int64_t				get_time(void);
+void					real_sleep(u_int64_t n);
+int						errormess(char *mess);
+int						message(t_phil *phil, int type);
+
+#endif
--- philo_one copy/srcs/eat.c
+++ philo_one copy/srcs/eat.c
@@ -12,24 +12,43 @@
 
 #include <philo.h>
 
-void	eat(t_phil *phil)
+bool	forks_ready(t_phil *phil)
 {
-	if (phil->name % 2)
-		pthread_mutex_lock(phil->f1);
-	pthread_mutex_lock(phil->f2);
-	message(phil, FORK);
-	if (!phil->name % 2)
-		pthread_mutex_lock(phil->f1);
-	if (!phil->info->someone_is_dead)
+	while (!phil->info->someone_is_dead)
 	{
+		pthread_mutex_lock(&phil->info->fork_check);
+		if (phil->info->n_forks[phil->name - 1] && 
+		phil->info->n_forks[phil->name % phil->info->num_phil])
+		{
+			phil->info->n_forks[phil->name - 1] = 0;
+			phil->info->n_forks[phil->name % phil->info->num_phil] = 0;
+			pthread_mutex_unlock(&phil->info->fork_check);
+			return true;
+		}
+		pthread_mutex_unlock(&phil->info->fork_check);
+	}
+	return false;
+}
+
+void	eat(t_phil *phil)
+{	
+	if (forks_ready(phil) && !phil->info->someone_is_dead)
+	{
+		pthread_mutex_lock(phil->f1);
+		pthread_mutex_lock(phil->f2);
+		message(phil, FORK);
 		pthread_mutex_lock(&phil->is_eating);
 		message(phil, FORK);
 		message(phil, EAT);
 		phil->last_eat = get_time();
 		real_sleep(phil->info->time_to_eat);
+		pthread_mutex_lock(&phil->info->fork_check);
+		phil->info->n_forks[phil->name - 1] = 1;
+		phil->info->n_forks[phil->name % phil->info->num_phil] = 1;
+		pthread_mutex_unlock(&phil->info->fork_check);
+		pthread_mutex_unlock(phil->f1);
+		pthread_mutex_unlock(phil->f2);
 		++phil->times_eaten;
 		pthread_mutex_unlock(&phil->is_eating);
 	}
-	pthread_mutex_unlock(phil->f1);
-	pthread_mutex_unlock(phil->f2);
 }
